932e8d9765a9e966f53e9eeacaa952ff9047d78c
simporechris <2468242@cegepoutaouais.qc.ca>
2025-09-29 09:10:03 -0400

    imcomplet

---
+++ test.py
@@ -0,0 +1,31 @@
+import pytest
+from debogage import roche_papier_ciseau
+
+def test_roche_papier_ciseau():
+    choix_joueur1 = "roche"
+    choix_joueur2 = "roche"
+    resultat_attendu = "Partie nulle!"
+    resultat_obtenu = roche_papier_ciseau(choix_joueur1, choix_joueur2)
+    assert resultat_obtenu == resultat_attendu
+
+
+def test_roche_papier_ciseau_1():
+    choix_joueur1 = "papier"
+    choix_joueur2 = "roche"
+    resultat_attendu = "Le gagnant est joueur1 avec papier, félicitation."
+    resultat_obtenu = roche_papier_ciseau(choix_joueur1, choix_joueur2)
+    assert resultat_obtenu == resultat_attendu
+
+def test_roche_papier_ciseau_2():
+    choix_joueur1 = "papier"
+    choix_joueur2 = "roche"
+    resultat_attendu = "Le gagnant est joueur1 avec papier, félicitation."
+    resultat_obtenu = roche_papier_ciseau(choix_joueur1, choix_joueur2)
+    assert resultat_obtenu == resultat_attendu
+
+def test_roche_papier_ciseau_3():
+    choix_joueur1 = "papier"
+    choix_joueur2 = "roche"
+    resultat_attendu = "Le gagnant est joueur1 avec papier, félicitation."
+    resultat_obtenu = roche_papier_ciseau(choix_joueur1, choix_joueur2)
+    assert resultat_obtenu == resultat_attendu
